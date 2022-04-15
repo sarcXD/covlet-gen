8cdc24cb9fd44c4b5ee4e7d00f3a44b9214caecd
talha <sarcxd@gmail.com>
2022-04-15 18:33:05 +0500

    Tuned pdf format and settings for text to pdf conversion

--- main.py
+++ main.py
@@ -35,26 +35,17 @@ def generate_covlet(role, keywords):
     return generate(keywords, template)
 
 def text_to_pdf(text, filename):
-    a4_width_mm = 210
-    pt_to_mm = 0.35
-    fontsize_pt = 10
-    fontsize_mm = fontsize_pt * pt_to_mm
-    margin_bottom_mm = 10
-    char_width_mm = 7 * pt_to_mm
-    width_text = a4_width_mm / char_width_mm
-
-    pdf = FPDF(orientation='p', unit='mm', format='A4')
-    pdf.set_auto_page_break(True, margin=margin_bottom_mm)
+    pdf = FPDF(orientation='p', format='A4')
+    pdf.set_margins(25,25)
     pdf.add_page()
-    pdf.set_font(family='Courier', size=fontsize_pt)
+    pdf.set_font('Arial', '', 11)
     splitted = text.split('\n')
 
     for line in splitted:
-        lines = textwrap.wrap(line, width_text)
-        if len(lines) == 0:
-            pdf.ln()
+        lines = textwrap.wrap(line, 95)
         for wrap in lines:
-            pdf.cell(0, fontsize_mm, wrap, ln=1)
+            pdf.cell(0, 5, wrap, ln=1)
+        pdf.ln()
     
     pdf.output(filename, 'F')
 
